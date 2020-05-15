e99e5ab04469418f81ea1f5a1b134639e760ec20
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2020-05-15 18:29:30 +0700

    moved to screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'globals.dart';
 
 void main() => runApp(Root());
 
 class Root extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
+    // SystemChrome.setPreferredOrientations([
+    //   DeviceOrientation.portraitUp,
+    // ]);
     return MaterialApp(
       title: 'CytoGenesis',
-      home: Scaffold(
-        appBar: EmptyAppBar(),
-        body: _BodyColumn(),
-      ),
+      // home: Scaffold(
+      //   appBar: EmptyAppBar(),
+      //   body: _BodyColumn(),
+      // ),
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => Scaffold(
+              appBar: EmptyAppBar(),
+              body: _BodyColumn(),
+            ),
+      },
     );
   }
-}
-
-// Handle status bar bullshit. Source: https://github.com/flutter/flutter/issues/4518#issuecomment-480115134
-class EmptyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  static const double _defaultElevation = 4.0;
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData themeData = Theme.of(context);
-    final AppBarTheme appBarTheme = AppBarTheme.of(context);
-    final Brightness brightness =
-        appBarTheme.brightness ?? themeData.primaryColorBrightness;
-    final SystemUiOverlayStyle overlayStyle = brightness == Brightness.dark
-        ? SystemUiOverlayStyle.light
-        : SystemUiOverlayStyle.dark;
-
-    return Semantics(
-      container: true,
-      child: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: overlayStyle,
-        child: Material(
-          color: appBarTheme.color ?? themeData.primaryColor,
-          elevation: appBarTheme.elevation ?? _defaultElevation,
-          child: Semantics(
-            explicitChildNodes: true,
-            child: Container(),
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Size get preferredSize => Size(0.0, 0.0);
 }
 
 class _TopRow extends StatelessWidget {

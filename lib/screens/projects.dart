e43e0e3d337da929aa8a4ffaa3849b1c3281aca7
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2020-05-15 18:40:03 +0700

    Update moving to screens

---
+++ lib/screens/projects.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../globals.dart';
+
+class ProjectScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+    return Scaffold(
+      appBar: EmptyAppBar(),
+      body: BodyColumn(),
+    );
+  }
+}
+
+class BodyColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: _TopRow(),
+        ),
+        Expanded(
+          flex: 10,
+          child: _ProjectRecent(),
+        ),
+      ],
+    );
+  }
+}
+
+class _TopRow extends StatelessWidget {
+  Expanded _buildTopRowChild(String text) => Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 4,
+              color: Colors.black87,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(color: Colors.black87),
+              textDirection: TextDirection.ltr,
+            ),
+          ),
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        _buildTopRowChild('Create New'),
+        _buildTopRowChild('Open'),
+      ],
+    );
+  }
+}
+
+class _ProjectRecent extends StatefulWidget {
+  @override
+  _ProjectRecentState createState() => _ProjectRecentState();
+}
+
+class _ProjectRecentState extends State<_ProjectRecent> {
+  int _count = 0;
+
+  void _increaseCount() {
+    setState(() {
+      _count++;
+    });
+  }
+
+  void _decreaseCount() {
+    setState(() {
+      _count--;
+    });
+  }
+
+  void _setCountZero() {
+    setState(() {
+      _count = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(8),
+      children: <Widget>[
+        Container(
+          height: 50,
+          color: Colors.amber[1000],
+          child: Center(child: Text('Count: $_count')),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[600],
+          child: FlatButton(
+            child: Center(child: Text('Increase Count')),
+            onPressed: _increaseCount,
+          ),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[500],
+          child: FlatButton(
+            child: Center(child: Text('Decrease Count')),
+            onPressed: _decreaseCount,
+          ),
+        ),
+        Container(
+          height: 50,
+          color: Colors.amber[100],
+          child: FlatButton(
+            child: Center(child: Text('Set to 0')),
+            onPressed: _setCountZero,
+          ),
+        ),
+      ],
+    );
+  }
+}

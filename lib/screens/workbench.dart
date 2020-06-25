b4eac6a26f76bac00488de22e5919af29072ddb3
TigorLazuard <tigor.hutasuhut@gmail.com>
2020-06-25 17:06:28 +0700

    Update arrow button

--- lib/screens/workbench.dart
+++ lib/screens/workbench.dart
@@ -13,7 +13,8 @@ class _WorkbbenchScreenState extends State<WorkbenchScreen> {
     super.initState();
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
-    SystemChrome.setEnabledSystemUIOverlays([]);
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
   }
 
   @override
@@ -33,13 +34,151 @@ class _WorkbbenchScreenState extends State<WorkbenchScreen> {
     // Still need to return scaffold because base app is Material...
     return Scaffold(
       appBar: Null(),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
+      body: BaseWorkbenchLayout(),
+    );
+  }
+}
+
+class BaseWorkbenchLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[TopArea(), BottomArea()],
+    );
+  }
+}
+
+class TopArea extends StatelessWidget {
+  const TopArea({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      child: TopAreaContent(),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Colors.black,
         ),
+      ),
+    );
+  }
+}
+
+class TopAreaContent extends StatelessWidget {
+  const TopAreaContent({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        BurgerMenuButton(),
+        LeftArrowButton(),
+        PageMeta(),
+        RightArrowButton()
+      ],
+    );
+  }
+}
+
+class RightArrowButton extends StatelessWidget {
+  const RightArrowButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Right Arrow'),
+      ),
+      padding: EdgeInsets.only(right: 5, left: 5),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
+
+class PageMeta extends StatelessWidget {
+  const PageMeta({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Page Meta'),
+      ),
+      margin: EdgeInsets.only(right: 5, left: 5),
+    );
+  }
+}
+
+class BurgerMenuButton extends StatelessWidget {
+  const BurgerMenuButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Burger Menu'),
+      ),
+      margin: EdgeInsets.only(right: 5, left: 5),
+    );
+  }
+}
+
+class LeftArrowButton extends StatelessWidget {
+  const LeftArrowButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Left Arrow'),
+      ),
+      padding: EdgeInsets.only(right: 5, left: 5),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
+
+class BottomArea extends StatelessWidget {
+  const BottomArea({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
         child: Center(
-          child: Text(
-            "Workbench",
+          child: Text('Bottom'),
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1,
+            color: Colors.red,
           ),
         ),
       ),
